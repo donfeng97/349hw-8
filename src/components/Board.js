1b2cbe9cbbea9bdeb0e6bc30023bdef2ad2c18b9
ice <donfeng97@csu.fullerton.edu>
2020-05-09 22:24:29 -0700

    finished

---
+++ src/components/Board.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Square from './Square';
+
+export default class Board extends Component {
+    renderSquare(i){
+      return (
+    <Square
+      isWinning={this.props.winningSquares.includes(i)}
+      key={"square " + i}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+    />
+  );
+}
+
+renderSquares(n) {
+  let squares = [];
+  for (let i = n; i < n + 3; i++) {
+    squares.push(this.renderSquare(i));
+  }
+  return squares;
+}
+
+renderRows(i) {
+  return <div className="board-row">{this.renderSquares(i)}</div>;
+}
+
+render() {
+  return (
+    <div>
+      {this.renderRows(0)}
+      {this.renderRows(3)}
+      {this.renderRows(6)}
+    </div>
+  );
+}
+}
---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Square(props) {
+    //return (
+    //    <button className="square" onClick={props.onClick}>
+    //        {props.value}
+    //    </button>
+    //)
+
+    return (
+    <button
+      className={"square " + (props.isWinning ? "square--winning" : null)}
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
